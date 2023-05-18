1bebf7f5ce5294e471f47a0992daab5932bf405f
BeccaFi <112644718+BeccaFi@users.noreply.github.com>
2023-05-18 18:17:06 +0200

    Added subtraction calculator functionality + tests

---
+++ controllers/calcSubController.js
@@ -0,0 +1,10 @@
+const { CalcSub } = require('../calculator/calcSub');
+
+function calcSubCon(req, res) {
+  const val1 = Number(req.query.val1);
+  const val2 = Number(req.query.val2);
+  const calc = new CalcSub(val1, val2);
+  return res.send(calc.getResult());
+}
+
+module.exports = calcSubCon;
