23d032abb0c9e4a6142abda5dbaf95dfe5d8ab07
BeccaFi <112644718+BeccaFi@users.noreply.github.com>
2023-05-22 13:35:14 +0200

    Corrected supertests

--- supertest/addTest.js
+++ supertest/addTest.js
@@ -5,16 +5,16 @@ const app = require('../app');
 // const { CalcAdd } = require('../calculator/calcAdd');
 
 describe('Checking for correct url query inputs', function () {
-  it('Should return status 200 when both values included', function (done) {
-    request(app).get('/calc/add?val1=2&val2=2').expect(200, done());
+  it('Should return status 200 when both values included', function () {
+    return request(app).get('/calc/add?val1=2&val2=2').expect(200);
   });
 
-  it('Should return status 400 when two values not included', function (done) {
-    request(app).get('/calc/add?val1=2').expect(400, done());
+  it('Should return status 400 when two values not included', function () {
+    return request(app).get('/calc/add?val1=2').expect(400);
   });
 
-  it('Should equal 18 when inserting 5 + 13', function (done) {
-    request(app).get('/calc/add?val1=5&val2=13').expect('5 + 13 = 18', done());
+  it('Should equal 18 when inserting 5 + 13', function () {
+    return request(app).get('/calc/add?val1=5&val2=13').expect('5 + 13 = 18');
   });
 });
 
--- supertest/divTest.js
+++ supertest/divTest.js
@@ -5,16 +5,16 @@ const app = require('../app');
 // const { CalcDiv } = require('../calculator/calcDiv');
 
 describe('Checking for correct url query inputs', function () {
-  it('Should return status 200 when both values included', function (done) {
-    request(app).get('/calc/div?val1=50&val2=2').expect(200, done());
+  it('Should return status 200 when both values included', function () {
+    return request(app).get('/calc/div?val1=50&val2=2').expect(200);
   });
 
-  it('Should return status 400 when two values not included', function (done) {
-    request(app).get('/calc/add?val1=103').expect(400, done());
+  it('Should return status 400 when two values not included', function () {
+    return request(app).get('/calc/div?val1=103').expect(400);
   });
 
-  it('Should equal 12 when inserting 36 / 3', function (done) {
-    request(app).get('/calc/add?val1=5&val2=13').expect('36 / 3 = 12', done());
+  it('Should equal 12 when inserting 36 / 3', function () {
+    return request(app).get('/calc/div?val1=36&val2=3').expect('36 / 3 = 12');
   });
 });
 
