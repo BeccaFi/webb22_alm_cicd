c5897c4f26b440eb2df8f1eeaaf373fd5cd120d2
BeccaFi <112644718+BeccaFi@users.noreply.github.com>
2023-05-23 11:47:23 +0200

    Added nyc coverage and added missing tests

---
+++ supertest/subSupertest.js
@@ -0,0 +1,16 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('Checking for correct url query inputs', function () {
+  it('Should return status 200 when both values included', function () {
+    return request(app).get('/calc/sub?val1=2&val2=2').expect(200);
+  });
+
+  it('Should return status 400 when two values not included', function () {
+    return request(app).get('/calc/sub?val1=2').expect(400);
+  });
+
+  it('Should equal 25 when inserting 60 - 35', function () {
+    return request(app).get('/calc/sub?val1=60&val2=35').expect('60 - 35 = 25');
+  });
+});
---
+++ validations/calcMultValidation.js
@@ -0,0 +1,8 @@
+const joi = require('joi');
+
+const schema = joi.object({
+  val1: joi.number().required(),
+  val2: joi.number().required(),
+});
+
+module.exports.calcMultValidation = (query) => schema.validate(query);
