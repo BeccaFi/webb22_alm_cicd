b3cf1641a1ad7c69c4e9f3c427b01162a062c658
BeccaFi <112644718+BeccaFi@users.noreply.github.com>
2023-05-23 15:39:05 +0200

    Added incorrect expect-status to calc test

--- supertest/calcSupertest.js
+++ supertest/calcSupertest.js
@@ -3,7 +3,7 @@ const app = require('../app');
 
 describe('Checking response when accessing calculator', function () {
   it('Should return status 200 when only /calc is visited', function () {
-    return request(app).get('/calc').expect(200);
+    return request(app).get('/calc').expect(400);
   });
 
   it('Content length should be 167 if message is displayed correctly', function () {
